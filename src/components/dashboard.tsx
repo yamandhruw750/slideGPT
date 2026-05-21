d2eafb113b4a61ac824c63a8d112e2aac7b9d725
Yaman Dhruw <yamandhruw750@gmail.com>
2026-05-21 17:33:17 +0530

    change dashboard 365 : md:grid-cols-2 xl:grid-cols-3

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -362,7 +362,7 @@ export function DashboardSection() {
             </Button>
           </div> */}
 
-          <div className="grid gap-4 md:grid-cols-2 xl:grid-cols-3">
+          <div className="grid gap-4 md:grid-cols-1 xl:grid-cols-1">
             <PresentationListSection
               presentations={presentations}
               isPending={listPending}
